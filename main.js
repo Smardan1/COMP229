caea921c58cdfd1a2a344780949ed916095fd876
Sulaiman Mardanyar <smardan1@my.centennialcollege.ca>
2023-09-11 12:04:59 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+//import {halfOf, multiply} from './lib.js';
+//console.log(halfOf(84));
+//console.log(multiply(21,2));
+
+//Arrow Function
+const sum1 = (a, b) => a + b;
+let result = sum1(1, 2);
+console.log(result);
